769725ff74a6e3b375921d1fafe1511bd8f8e4cc
Tatiana Malygina <merlettaia@gmail.com>
2015-01-14 07:39:43 +0300

    changed stepCV - now works for simplest linear models

--- SL/hw3/stepCV.R
+++ SL/hw3/stepCV.R
@@ -1,3 +1,106 @@
+# from file MASS/R/add.R
+# dropterm method redefined
+addterm2 <-
+  function(object, ...) UseMethod("addterm2")
+
+addterm2.default <-
+  function(object, scope, scale = 0, test = c("none"),
+           k = 2, sorted = FALSE, trace = FALSE, ...)
+  {
+    if(missing(scope) || is.null(scope)) stop("no terms in scope")
+    if(!is.character(scope))
+      scope <- add.scope(object, update.formula(object, scope))
+    if(!length(scope))
+      stop("no terms in scope for adding to object")
+    #     newform <- update.formula(object,
+    #                               paste(". ~ . +", paste(scope, collapse="+")))
+    #     data <- model.frame(update(object, newform)) # remove NAs
+    #     object <- update(object, data = data)
+    ns <- length(scope)
+    ans <- matrix(nrow = ns + 1L, ncol = 2L,
+                  dimnames = list(c("<none>", scope), c("rank", "performance")))
+    ans[1L,  ] <- extractCV(object, scale, k = k, ...)
+    n0 <- nobs(object, use.fallback = TRUE)
+    env <- environment(formula(object))
+    for(i in seq_len(ns)) {
+      tt <- scope[i]
+      if(trace) {
+        message(gettextf("trying + %s", tt), domain = NA)
+        utils::flush.console()
+      }
+      nfit <- update(object, as.formula(paste("~ . +", tt)),
+                     evaluate = FALSE)
+      nfit <- try(eval(nfit, envir = env), silent = TRUE)
+      ans[i + 1L, ] <- if (!inherits(nfit, "try-error")) {
+        nnew <- nobs(nfit, use.fallback = TRUE)
+        if (all(is.finite(c(n0, nnew))) && nnew != n0)
+          stop("number of rows in use has changed: remove missing values?")
+        extractCV(nfit, scale, k = k, ...)
+      } else NA_real_
+    }
+    dfs <- ans[, 1L] - ans[1L, 1L]
+    dfs[1L] <- NA
+    aod <- data.frame(Df = dfs, AIC = ans[, 2L])
+    o <- if(sorted) order(aod$AIC) else seq_along(aod$AIC)
+    #test <- match.arg(test)
+    aod <- aod[o, ]
+    head <- c("Single term additions", "\nModel:", deparse(formula(object)))
+    if(scale > 0)
+      head <- c(head, paste("\nscale: ", format(scale), "\n"))
+    class(aod) <- c("anova", "data.frame")
+    attr(aod, "heading") <- head
+    aod
+  }
+
+dropterm2 <- function(object, ...) UseMethod("dropterm2")
+
+dropterm2.default <-
+  function(object, scope, scale = 0, test = c("none"),
+           k = 2, sorted = FALSE, trace = FALSE, ...)
+  {
+    tl <- attr(terms(object), "term.labels")
+    if(missing(scope)) scope <- drop.scope(object)
+    else {
+      if(!is.character(scope))
+        scope <- attr(terms(update.formula(object, scope)), "term.labels")
+      if(!all(match(scope, tl, 0L)))
+        stop("scope is not a subset of term labels")
+    }
+    ns <- length(scope)
+    ans <- matrix(nrow = ns + 1L, ncol = 2L,
+                  dimnames =  list(c("<none>", scope), c("rank", "performance")))
+    
+    ans[1,  ] <- extractCV(object, scale, k = k, ...)
+    n0 <- nobs(object, use.fallback = TRUE)
+    env <- environment(formula(object))
+    for(i in seq_len(ns)) {
+      tt <- scope[i]
+      if(trace) {
+        message(gettextf("trying - %s", tt), domain = NA)
+        utils::flush.console()
+      }
+      nfit <- update(object, as.formula(paste("~ . -", tt)),
+                     evaluate = FALSE)
+      nfit <- eval(nfit, envir=env) # was  eval.parent(nfit)
+      ans[i+1, ] <- extractCV(nfit, scale, k = k, ...)
+      nnew <- nobs(nfit, use.fallback = TRUE)
+      if(all(is.finite(c(n0, nnew))) && nnew != n0)
+        stop("number of rows in use has changed: remove missing values?")
+    }
+    dfs <- ans[1L , 2L] - ans[, 2L]
+    dfs[1L] <- NA
+    aod <- data.frame(Df = dfs, rank=ans[, 1], CV = ans[,2])
+    o <- if(sorted) order(aod$CV) else seq_along(aod$CV)
+    test <- match.arg(test)
+    aod <- aod[o, ]
+    head <- c("Single term deletions", "\nModel:", deparse(formula(object)))
+    if(scale > 0)
+      head <- c(head, paste("\nscale: ", format(scale), "\n"))
+    class(aod) <- c("anova", "data.frame")
+
+    attr(aod, "heading") <- head
+    aod
+  }
 # file MASS/R/stepAIC.R
 # copyright (C) 1994-2007 W. N. Venables and B. D. Ripley
 #
@@ -14,7 +117,7 @@
 #  A copy of the GNU General Public License is available at
 #  http://www.r-project.org/Licenses/
 #
-stepAIC <-
+stepCV <-
   function(object, scope, scale = 0,
            direction = c("both", "backward", "forward"),
            trace = 1, keep = NULL, steps = 1000, use.start = FALSE, k = 2, ...)
@@ -22,7 +125,7 @@ stepAIC <-
     mydeviance <- function(x, ...)
     {
         dev <- deviance(x)
-        if(!is.null(dev)) dev else extractAIC(x, k=0)[2L]
+        if(!is.null(dev)) dev else extractCV(x, k=0)[2L]
     }
 
     cut.string <- function(string)
@@ -44,23 +147,20 @@ stepAIC <-
     step.results <- function(models, fit, object, usingCp=FALSE)
     {
         change <- sapply(models, "[[", "change")
-        rd <- sapply(models, "[[", "deviance")
-        dd <- c(NA, abs(diff(rd)))
-        rdf <- sapply(models, "[[", "df.resid")
-        ddf <- c(NA, abs(diff(rdf)))
-        AIC <- sapply(models, "[[", "AIC")
+        rank <- sapply(models, "[[", "rank")
+        performance <- sapply(models, "[[", "performance")
+        ddf <- c(NA, abs(diff(performance)))
+        CV <- sapply(models, "[[", "CV")
         heading <- c("Stepwise Model Path \nAnalysis of Deviance Table",
                      "\nInitial Model:", deparse(formula(object)),
                      "\nFinal Model:", deparse(formula(fit)),
                      "\n")
         aod <-
             if(usingCp)
-                data.frame(Step = change, Df = ddf, Deviance = dd,
-                           "Resid. Df" = rdf, "Resid. Dev" = rd,
-                           Cp = AIC, check.names = FALSE)
-            else data.frame(Step = change, Df = ddf, Deviance = dd,
-                            "Resid. Df" = rdf, "Resid. Dev" = rd,
-                            AIC = AIC, check.names = FALSE)
+                data.frame(Step = change, performance = performance, rank = rank,
+                           Cp = CV, check.names = FALSE)
+            else data.frame(Step = change, performance = performance, rank = rank,
+                            CV = CV, check.names = FALSE)
         attr(aod, "heading") <- heading
         class(aod) <- c("Anova", "data.frame")
         fit$anova <- aod
@@ -78,7 +178,7 @@ stepAIC <-
     backward <- direction == "both" | direction == "backward"
     forward <- direction == "both" | direction == "forward"
     if(missing(scope)) {
-	fdrop <- numeric()
+	      fdrop <- numeric()
         fadd <- attr(Terms, "factors")
         if(md) forward <- FALSE
     } else {
@@ -98,27 +198,26 @@ stepAIC <-
     if(!is.null(keep)) keep.list <- vector("list", steps)
     n <- nobs(object, use.fallback = TRUE)  # might be NA
     fit <- object
-    bAIC <- extractAIC(fit, scale, k = k, ...)
-    edf <- bAIC[1L]
-    bAIC <- bAIC[2L]
-    if(is.na(bAIC))
-        stop("AIC is not defined for this model, so 'stepAIC' cannot proceed")
-    if(bAIC == -Inf)
-        stop("AIC is -infinity for this model, so 'stepAIC' cannot proceed")
+    resCV <- extractCV(fit, scale, k = k, ...)
+	  rank <- resCV[1L]
+	  performance <- resCV[2L]
+    if(is.na(performance))
+        stop("performance is not defined for this model, so 'stepCV' cannot proceed")
+    if(performance == -Inf)
+        stop("performance is -infinity for this model, so 'stepCV' cannot proceed")
    nm <- 1
     Terms <- terms(fit)
     if(trace) {
-        cat("Start:  AIC=", format(round(bAIC, 2)), "\n",
+        cat("Start:  CV.performance=", format(round(performance, 2)), "\n",
             cut.string(deparse(formula(fit))), "\n\n", sep='')
 	utils::flush.console()
     }
-    models[[nm]] <- list(deviance = mydeviance(fit), df.resid = n - edf,
-                         change = "", AIC = bAIC)
-    if(!is.null(keep)) keep.list[[nm]] <- keep(fit, bAIC)
+	  models[[nm]] <- list(rank = rank, change = "", performance = performance, CV=performance)
+    if(!is.null(keep)) keep.list[[nm]] <- keep(fit, performance)
     usingCp <- FALSE
     while(steps > 0) {
         steps <- steps - 1
-        AIC <- bAIC
+        CV <- performance
         ffac <- attr(Terms, "factors")
         ## don't drop strata terms
         if(!is.null(sp <- attr(Terms, "specials")) &&
@@ -127,18 +226,18 @@ stepAIC <-
         aod <- NULL
         change <- NULL
         if(backward && length(scope$drop)) {
-            aod <- dropterm(fit, scope$drop, scale = scale,
+            aod <- dropterm2(fit, scope$drop, scale = scale,
                             trace = max(0, trace - 1), k = k, ...)
             rn <- row.names(aod)
             row.names(aod) <- c(rn[1L], paste("-", rn[-1L], sep=" "))
             ## drop all zero df terms first.
             if(any(aod$Df == 0, na.rm=TRUE)) {
                 zdf <- aod$Df == 0 & !is.na(aod$Df)
-                nc <- match(c("Cp", "AIC"), names(aod))
+                nc <- match(c("Cp", "CV"), names(aod))
                 nc <- nc[!is.na(nc)][1L]
                 ch <- abs(aod[zdf, nc] - aod[1, nc]) > 0.01
                 if(any(is.finite(ch) & ch)) {
-                    warning("0 df terms are changing AIC")
+                    warning("0 df terms are changing CV")
                     zdf <- zdf[!ch]
                 }
                 ## drop zero df terms first: one at time since they
@@ -149,7 +248,7 @@ stepAIC <-
         }
         if(is.null(change)) {
             if(forward && length(scope$add)) {
-                aodf <- addterm(fit, scope$add, scale = scale,
+                aodf <- addterm2(fit, scope$add, scale = scale,
                                 trace = max(0, trace - 1), k = k, ...)
                 rn <- row.names(aodf)
                 row.names(aodf) <- c(rn[1L], paste("+", rn[-1L], sep=" "))
@@ -163,13 +262,13 @@ stepAIC <-
             nzdf <- if(!is.null(aod$Df)) aod$Df != 0 | is.na(aod$Df)
             aod <- aod[nzdf, ]
             if(is.null(aod) || ncol(aod) == 0) break
-            nc <- match(c("Cp", "AIC"), names(aod))
+            nc <- match(c("Cp", "CV"), names(aod))
             nc <- nc[!is.na(nc)][1L]
-            o <- order(aod[, nc])
+            o <- order(aod[, nc], decreasing =TRUE)
             if(trace) {
-		print(aod[o,  ])
-		utils::flush.console()
-	    }
+          		print(aod[o,  ])
+          		utils::flush.console()
+          	    }
             if(o[1L] == 1) break
             change <- rownames(aod)[o[1L]]
         }
@@ -181,46 +280,34 @@ stepAIC <-
         if(all(is.finite(c(n, nnew))) && nnew != n)
             stop("number of rows in use has changed: remove missing values?")
         Terms <- terms(fit)
-        bAIC <- extractAIC(fit, scale, k = k, ...)
-        edf <- bAIC[1L]
-        bAIC <- bAIC[2L]
+        resCV <- extractCV(fit, scale, k = k, ...)# was bAIC
+        #edf <- bAIC[1L]
+        #bAIC <- bAIC[2L]
+        rank <- resCV[1L]
+        performance <- resCV[2L]
         if(trace) {
-            cat("\nStep:  AIC=", format(round(bAIC, 2)), "\n",
+            cat("\nStep:  CV.performance=", format(round(performance, 2)), "\n",
                 cut.string(deparse(formula(fit))), "\n\n", sep='')
 	    utils::flush.console()
 	}
         ## add a tolerance as dropping 0-df terms might increase AIC slightly
-        if(bAIC >= AIC + 1e-7) break
+        if(performance >= CV + 1e-7) break
         nm <- nm + 1
         models[[nm]] <-
-            list(deviance = mydeviance(fit), df.resid = n - edf,
-                 change = change, AIC = bAIC)
-        if(!is.null(keep)) keep.list[[nm]] <- keep(fit, bAIC)
+            list(rank = rank, performance=performance,
+                 change = change, CV = performance)
+        if(!is.null(keep)) keep.list[[nm]] <- keep(fit, performance)
     }
     if(!is.null(keep)) fit$keep <- re.arrange(keep.list[seq(nm)])
     step.results(models = models[seq(nm)], fit, object, usingCp)
 }
 
-extractAIC.loglm <- function(fit, scale, k = 2, ...)
-{
-    edf <- fit$n - fit$df
-    c(edf,  fit$deviance + k * edf)
-}
+extractCV <- function(object, ...) UseMethod("extractCV")
 
-extractAIC.lme <- function(fit, scale, k = 2, ...)
+extractCV.lm <- function(fit, scale, k = 2, ...)
 {
-    if(fit$method != "ML") stop("AIC undefined for REML fit")
-    res <- logLik(fit)
-    edf <- attr(res, "df")
-    c(edf,  -2*res + k * edf)
-}
-
-extractAIC.gls <- function(fit, scale, k = 2, ...)
-{
-    if(fit$method != "ML") stop("AIC undefined for REML fit")
-    res <- logLik(fit)
-    edf <- attr(res, "df")
-    c(edf,  -2*res + k * edf)
+  t <- tune(lm, formula(fit$terms), data = fit$model)
+  c(t$best.model$rank, t$best.performance)
 }
 
 terms.gls <- terms.lme <- function(x, ...) terms(formula(x), ...)
--- SL/hw3/task4.md
+++ SL/hw3/task4.md
@@ -12,338 +12,115 @@ library(e1071)
 
 
 ```r
-my_data <- read.table("data/seeds_dataset.txt")#, sep = ",", comment.char = "#")
-print(colnames(my_data))
+source("stepCV.R")
 ```
 
-```
-## [1] "V1" "V2" "V3" "V4" "V5" "V6" "V7" "V8"
-```
+Пример
 
 
 ```r
-quine.hi <- aov(log(Days + 2.5) ~ .^4, quine)
-quine.nxt <- update(quine.hi, . ~ . - Eth:Sex:Age:Lrn)
-quine.stp <- stepAIC(quine.nxt,
-    scope = list(upper = ~Eth*Sex*Age*Lrn, lower = ~1),
-    trace = FALSE)
-quine.stp$anova
+l <- lm(y~. , epil)
+stepCV(l, trace = TRUE)
 ```
 
 ```
-## Stepwise Model Path 
-## Analysis of Deviance Table
+## Start:  CV.performance=64.32
+## y ~ trt + base + age + V4 + subject + period + lbase + lage
 ## 
-## Initial Model:
-## log(Days + 2.5) ~ Eth + Sex + Age + Lrn + Eth:Sex + Eth:Age + 
-##     Eth:Lrn + Sex:Age + Sex:Lrn + Age:Lrn + Eth:Sex:Age + Eth:Sex:Lrn + 
-##     Eth:Age:Lrn + Sex:Age:Lrn
+##                  Df rank      CV
+## - base    -40.80114    8 103.797
+## - lbase    -5.87600    8  68.872
+## - subject  -1.12091    8  64.117
+## <none>                 9  62.996
+## - V4        0.21232    8  62.783
+## - lage      0.86694    8  62.129
+## - age       1.00570    8  61.990
+## - trt       1.36480    8  61.631
+## - period    2.90006    8  60.096
 ## 
-## Final Model:
-## log(Days + 2.5) ~ Eth + Sex + Age + Lrn + Eth:Sex + Eth:Age + 
-##     Eth:Lrn + Sex:Age + Sex:Lrn + Age:Lrn + Eth:Sex:Lrn + Eth:Age:Lrn
-## 
-## 
-##            Step Df Deviance Resid. Df Resid. Dev       AIC
-## 1                                 120   64.09900 -68.18396
-## 2 - Eth:Sex:Age  3 0.973869       123   65.07287 -71.98244
-## 3 - Sex:Age:Lrn  2 1.526754       125   66.59962 -72.59652
-```
-
-```r
-cpus1 <- cpus
-for(v in names(cpus)[2:7])
-  cpus1[[v]] <- cut(cpus[[v]], unique(quantile(cpus[[v]])),
-                    include.lowest = TRUE)
-cpus0 <- cpus1[, 2:8]  # excludes names, authors' predictions
-cpus.samp <- sample(1:209, 100)
-cpus.lm <- lm(log10(perf) ~ ., data = cpus1[cpus.samp,2:8])
-cpus.lm2 <- stepAIC(cpus.lm, trace = FALSE)
-cpus.lm2$anova
-```
-
-```
-## Stepwise Model Path 
-## Analysis of Deviance Table
-## 
-## Initial Model:
-## log10(perf) ~ syct + mmin + mmax + cach + chmin + chmax
-## 
-## Final Model:
-## log10(perf) ~ mmax + cach + chmin + chmax
-## 
-## 
-##     Step Df   Deviance Resid. Df Resid. Dev       AIC
-## 1                             82   2.989974 -314.9906
-## 2 - mmin  3 0.09329905        85   3.083273 -317.9179
-## 3 - syct  3 0.11044221        88   3.193715 -320.3985
-```
-
-```r
-example(birthwt)
-```
-
-```
-## 
-## brthwt> bwt <- with(birthwt, {
-## brthwt+ race <- factor(race, labels = c("white", "black", "other"))
-## brthwt+ ptd <- factor(ptl > 0)
-## brthwt+ ftv <- factor(ftv)
-## brthwt+ levels(ftv)[-(1:2)] <- "2+"
-## brthwt+ data.frame(low = factor(low), age, lwt, race, smoke = (smoke > 0),
-## brthwt+            ptd, ht = (ht > 0), ui = (ui > 0), ftv)
-## brthwt+ })
-## 
-## brthwt> options(contrasts = c("contr.treatment", "contr.poly"))
-## 
-## brthwt> glm(low ~ ., binomial, bwt)
-## 
-## Call:  glm(formula = low ~ ., family = binomial, data = bwt)
-## 
-## Coefficients:
-## (Intercept)          age          lwt    raceblack    raceother  
-##     0.82302     -0.03723     -0.01565      1.19241      0.74068  
-##   smokeTRUE      ptdTRUE       htTRUE       uiTRUE         ftv1  
-##     0.75553      1.34376      1.91317      0.68020     -0.43638  
-##       ftv2+  
-##     0.17901  
-## 
-## Degrees of Freedom: 188 Total (i.e. Null);  178 Residual
-## Null Deviance:	    234.7 
-## Residual Deviance: 195.5 	AIC: 217.5
-```
-
-```r
-birthwt.glm <- glm(low ~ ., family = binomial, data = bwt)
-birthwt.step <- stepAIC(birthwt.glm, trace = FALSE)
-birthwt.step$anova
-```
-
-```
-## Stepwise Model Path 
-## Analysis of Deviance Table
-## 
-## Initial Model:
-## low ~ age + lwt + race + smoke + ptd + ht + ui + ftv
-## 
-## Final Model:
-## low ~ lwt + race + smoke + ptd + ht + ui
-## 
-## 
-##    Step Df Deviance Resid. Df Resid. Dev      AIC
-## 1                         178   195.4755 217.4755
-## 2 - ftv  2 1.358185       180   196.8337 214.8337
-## 3 - age  1 1.017866       181   197.8516 213.8516
-```
-
-```r
-birthwt.step2 <- stepAIC(birthwt.glm, ~ .^2 + I(scale(age)^2)
-    + I(scale(lwt)^2), trace = FALSE)
-birthwt.step2$anova
-```
-
-```
-## Stepwise Model Path 
-## Analysis of Deviance Table
-## 
-## Initial Model:
-## low ~ age + lwt + race + smoke + ptd + ht + ui + ftv
-## 
-## Final Model:
-## low ~ age + lwt + smoke + ptd + ht + ui + ftv + age:ftv + smoke:ui
-## 
-## 
-##         Step Df  Deviance Resid. Df Resid. Dev      AIC
-## 1                               178   195.4755 217.4755
-## 2  + age:ftv  2 12.474896       176   183.0006 209.0006
-## 3 + smoke:ui  1  3.056805       175   179.9438 207.9438
-## 4     - race  2  3.129586       177   183.0734 207.0734
-```
-
-```r
-quine.nb <- glm.nb(Days ~ .^4, data = quine)
-quine.nb2 <- stepAIC(quine.nb)
-```
-
-```
-## Start:  AIC=1095.32
-## Days ~ (Eth + Sex + Age + Lrn)^4
-## 
-##                   Df    AIC
-## - Eth:Sex:Age:Lrn  2 1092.7
-## <none>               1095.3
-## 
-## Step:  AIC=1092.73
-## Days ~ Eth + Sex + Age + Lrn + Eth:Sex + Eth:Age + Eth:Lrn + 
-##     Sex:Age + Sex:Lrn + Age:Lrn + Eth:Sex:Age + Eth:Sex:Lrn + 
-##     Eth:Age:Lrn + Sex:Age:Lrn
-## 
-##               Df    AIC
-## - Eth:Sex:Age  3 1089.4
-## <none>           1092.7
-## - Eth:Sex:Lrn  1 1093.3
-## - Eth:Age:Lrn  2 1094.7
-## - Sex:Age:Lrn  2 1095.0
-## 
-## Step:  AIC=1089.41
-## Days ~ Eth + Sex + Age + Lrn + Eth:Sex + Eth:Age + Eth:Lrn + 
-##     Sex:Age + Sex:Lrn + Age:Lrn + Eth:Sex:Lrn + Eth:Age:Lrn + 
-##     Sex:Age:Lrn
-## 
-##               Df    AIC
-## <none>           1089.4
-## - Sex:Age:Lrn  2 1091.1
-## - Eth:Age:Lrn  2 1091.2
-## - Eth:Sex:Lrn  1 1092.5
-```
-
-```r
-quine.nb2$anova
-```
-
-```
-## Stepwise Model Path 
-## Analysis of Deviance Table
-## 
-## Initial Model:
-## Days ~ (Eth + Sex + Age + Lrn)^4
-## 
-## Final Model:
-## Days ~ Eth + Sex + Age + Lrn + Eth:Sex + Eth:Age + Eth:Lrn + 
-##     Sex:Age + Sex:Lrn + Age:Lrn + Eth:Sex:Lrn + Eth:Age:Lrn + 
-##     Sex:Age:Lrn
-## 
-## 
-##                Step Df   Deviance Resid. Df Resid. Dev      AIC
-## 1                                       118   167.4535 1095.324
-## 2 - Eth:Sex:Age:Lrn  2 0.09746244       120   167.5509 1092.728
-## 3     - Eth:Sex:Age  3 0.11060087       123   167.4403 1089.409
-```
-
-
-```r
-df <- mtcars
-f <- mpg ~ .
-l <- lm(f, df)
-(l.aic <- stepAIC(l, trace = TRUE))
-```
-
-```
-## Start:  AIC=70.9
-## mpg ~ cyl + disp + hp + drat + wt + qsec + vs + am + gear + carb
-## 
-##        Df Sum of Sq    RSS    AIC
-## - cyl   1    0.0799 147.57 68.915
-## - vs    1    0.1601 147.66 68.932
-## - carb  1    0.4067 147.90 68.986
-## - gear  1    1.3531 148.85 69.190
-## - drat  1    1.6270 149.12 69.249
-## - disp  1    3.9167 151.41 69.736
-## - hp    1    6.8399 154.33 70.348
-## - qsec  1    8.8641 156.36 70.765
-## <none>              147.49 70.898
-## - am    1   10.5467 158.04 71.108
-## - wt    1   27.0144 174.51 74.280
-## 
-## Step:  AIC=68.92
-## mpg ~ disp + hp + drat + wt + qsec + vs + am + gear + carb
-## 
-##        Df Sum of Sq    RSS    AIC
-## - vs    1    0.2685 147.84 66.973
-## - carb  1    0.5201 148.09 67.028
-## - gear  1    1.8211 149.40 67.308
-## - drat  1    1.9826 149.56 67.342
-## - disp  1    3.9009 151.47 67.750
-## - hp    1    7.3632 154.94 68.473
-## <none>              147.57 68.915
-## - qsec  1   10.0933 157.67 69.032
-## - am    1   11.8359 159.41 69.384
-## - wt    1   27.0280 174.60 72.297
-## 
-## Step:  AIC=66.97
-## mpg ~ disp + hp + drat + wt + qsec + am + gear + carb
-## 
-##        Df Sum of Sq    RSS    AIC
-## - carb  1    0.6855 148.53 65.121
-## - gear  1    2.1437 149.99 65.434
-## - drat  1    2.2139 150.06 65.449
-## - disp  1    3.6467 151.49 65.753
-## - hp    1    7.1060 154.95 66.475
-## <none>              147.84 66.973
-## - am    1   11.5694 159.41 67.384
-## - qsec  1   15.6830 163.53 68.200
-## - wt    1   27.3799 175.22 70.410
-## 
-## Step:  AIC=65.12
-## mpg ~ disp + hp + drat + wt + qsec + am + gear
-## 
-##        Df Sum of Sq    RSS    AIC
-## - gear  1     1.565 150.09 63.457
-## - drat  1     1.932 150.46 63.535
-## <none>              148.53 65.121
-## - disp  1    10.110 158.64 65.229
-## - am    1    12.323 160.85 65.672
-## - hp    1    14.826 163.35 66.166
-## - qsec  1    26.408 174.94 68.358
-## - wt    1    69.127 217.66 75.350
-## 
-## Step:  AIC=63.46
-## mpg ~ disp + hp + drat + wt + qsec + am
-## 
-##        Df Sum of Sq    RSS    AIC
-## - drat  1     3.345 153.44 62.162
-## - disp  1     8.545 158.64 63.229
-## <none>              150.09 63.457
-## - hp    1    13.285 163.38 64.171
-## - am    1    20.036 170.13 65.466
-## - qsec  1    25.574 175.67 66.491
-## - wt    1    67.572 217.66 73.351
-## 
-## Step:  AIC=62.16
-## mpg ~ disp + hp + wt + qsec + am
-## 
-##        Df Sum of Sq    RSS    AIC
-## - disp  1     6.629 160.07 61.515
-## <none>              153.44 62.162
-## - hp    1    12.572 166.01 62.682
-## - qsec  1    26.470 179.91 65.255
-## - am    1    32.198 185.63 66.258
-## - wt    1    69.043 222.48 72.051
-## 
-## Step:  AIC=61.52
-## mpg ~ hp + wt + qsec + am
-## 
-##        Df Sum of Sq    RSS    AIC
-## - hp    1     9.219 169.29 61.307
-## <none>              160.07 61.515
-## - qsec  1    20.225 180.29 63.323
-## - am    1    25.993 186.06 64.331
-## - wt    1    78.494 238.56 72.284
-## 
-## Step:  AIC=61.31
-## mpg ~ wt + qsec + am
-## 
-##        Df Sum of Sq    RSS    AIC
-## <none>              169.29 61.307
-## - am    1    26.178 195.46 63.908
-## - qsec  1   109.034 278.32 75.217
-## - wt    1   183.347 352.63 82.790
+## Step:  CV.performance=105.16
+## y ~ trt + age + V4 + subject + period + lbase + lage
 ```
 
 ```
 ## 
 ## Call:
-## lm(formula = mpg ~ wt + qsec + am, data = df)
+## lm(formula = y ~ trt + age + V4 + subject + period + lbase + 
+##     lage, data = epil)
 ## 
 ## Coefficients:
-## (Intercept)           wt         qsec           am  
-##       9.618       -3.917        1.226        2.936
+##  (Intercept)  trtprogabide           age            V4       subject  
+##     39.77134      -3.11864      -1.09672      -0.72316       0.06868  
+##       period         lbase          lage  
+##     -0.27119      10.24944      33.40300
 ```
+
+Можно сравнить результат с stepAIC:
 
 ```r
-(l.cv <- stepCV(df, l, trace = TRUE))
+stepAIC(l, trace = TRUE)
 ```
 
 ```
-## Error in eval(expr, envir, enclos): could not find function "stepCV"
+## Start:  AIC=963.38
+## y ~ trt + base + age + V4 + subject + period + lbase + lage
+## 
+##           Df Sum of Sq   RSS     AIC
+## - period   1       8.7 12969  961.53
+## - V4       1       9.3 12970  961.54
+## - age      1      67.3 13028  962.60
+## - trt      1     103.7 13064  963.26
+## - lage     1     104.4 13065  963.27
+## <none>                 12960  963.38
+## - subject  1     132.8 13093  963.78
+## - lbase    1    1376.2 14337  985.19
+## - base     1    9300.0 22260 1089.03
+## 
+## Step:  AIC=961.53
+## y ~ trt + base + age + V4 + subject + lbase + lage
+## 
+##           Df Sum of Sq   RSS     AIC
+## - age      1      67.3 13036  960.76
+## - V4       1      70.9 13040  960.82
+## - trt      1     103.7 13073  961.41
+## - lage     1     104.4 13074  961.43
+## <none>                 12969  961.53
+## - subject  1     132.8 13102  961.94
+## - lbase    1    1376.2 14345  983.34
+## - base     1    9300.0 22269 1087.12
+## 
+## Step:  AIC=960.76
+## y ~ trt + base + V4 + subject + lbase + lage
+## 
+##           Df Sum of Sq   RSS     AIC
+## - V4       1      70.9 13107  960.04
+## <none>                 13036  960.76
+## - trt      1     111.8 13148  960.77
+## - subject  1     130.0 13166  961.10
+## - lage     1     329.4 13366  964.64
+## - lbase    1    1431.0 14468  983.34
+## - base     1    9368.6 22405 1086.56
+## 
+## Step:  AIC=960.04
+## y ~ trt + base + subject + lbase + lage
+## 
+##           Df Sum of Sq   RSS     AIC
+## <none>                 13107  960.04
+## - trt      1     111.8 13219  960.04
+## - subject  1     130.0 13237  960.37
+## - lage     1     329.4 13437  963.89
+## - lbase    1    1431.0 14538  982.49
+## - base     1    9368.6 22476 1085.30
+```
+
+```
+## 
+## Call:
+## lm(formula = y ~ trt + base + subject + lbase + lage, data = epil)
+## 
+## Coefficients:
+##  (Intercept)  trtprogabide          base       subject         lbase  
+##     -10.2977       -2.7702        0.5570        0.0873       -7.7688  
+##         lage  
+##       5.4846
 ```
