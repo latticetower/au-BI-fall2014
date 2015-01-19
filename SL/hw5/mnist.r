4f2f272e4f7f583b7208a357ceba0df1ad35bff9
Tatiana Malygina <merlettaia@gmail.com>
2015-01-19 12:38:37 +0300

    finished preprocessor, added helper methods

--- SL/hw5/mnist.r
+++ SL/hw5/mnist.r
@@ -6,34 +6,72 @@
 # call:  show_digit(train$x[5,])   to see a digit.
 # brendan o'connor - gist.github.com/39760 - anyall.org
 
+load_image_file <- function(filename) {
+  ret = list()
+  f = file(filename,'rb')
+  readBin(f,'integer',n=1,size=4,endian='big')
+  ret$n = readBin(f,'integer',n=1,size=4,endian='big')
+  nrow = readBin(f,'integer',n=1,size=4,endian='big')
+  ncol = readBin(f,'integer',n=1,size=4,endian='big')
+
+  x = readBin(f,'integer',n=ret$n*nrow*ncol,size=1,signed=F)
+  ret$x = matrix(x, ncol=nrow*ncol, byrow=T)
+  close(f)
+  ret
+}
+
+load_image_file2 <- function(filename) {
+  ret = list()
+  f = file(filename,'rb')
+  readBin(f,'integer',n=1,size=4,endian='big')
+  ret$n = readBin(f,'integer',n=1,size=4,endian='big')
+  ret$nrow = readBin(f,'integer',n=1,size=4,endian='big')
+  ret$ncol = readBin(f,'integer',n=1,size=4,endian='big')
+  x = readBin(f,'integer',n=ret$n*ret$ncol*ret$nrow,size=1,signed=F)
+  ret$x = matrix(x, nrow=ret$n, byrow=T)
+  close(f)
+  ret
+}
+load_label_file <- function(filename) {
+  f = file(filename,'rb')
+  readBin(f,'integer',n=1,size=4,endian='big')
+  n = readBin(f,'integer',n=1,size=4,endian='big')
+  y = readBin(f,'integer',n=n,size=1,signed=F)
+  close(f)
+  y
+}
+
+save_image_file <- function(filename, data, n, ncol, nrow) {
+  f = file(filename,'wb')
+  writeBin(1, f, endian='big', size=4)
+  writeBin(n, f, endian='big', size=4)
+  writeBin(nrow, f, endian='big', size=4)
+  writeBin(ncol, f, endian='big', size=4)
+  print(as.vector(matrix(t(data[, -1]), nrow = 1, byrow=T)))
+  x = writeBin(as.vector(matrix(t(data[, -1]), nrow = 1)[1, ]),f, size=1)
+
+  #ret$x = matrix(x, ncol=nrow*ncol, byrow=T)
+  close(f)
+}
+
+save_label_file <- function(filename, n, v) {
+  f = file(filename,'wb')
+  writeBin(2, f,size=4,endian='big')
+  writeBin(n, f,size=4,endian='big')
+  print(as.vector(v))
+  writeBin(as.integer(as.vector(v)), f,size=1)
+  close(f)
+}
+
 load_mnist <- function() {
-  load_image_file <- function(filename) {
-    ret = list()
-    f = file(filename,'rb')
-    readBin(f,'integer',n=1,size=4,endian='big')
-    ret$n = readBin(f,'integer',n=1,size=4,endian='big')
-    nrow = readBin(f,'integer',n=1,size=4,endian='big')
-    ncol = readBin(f,'integer',n=1,size=4,endian='big')
-    x = readBin(f,'integer',n=ret$n*nrow*ncol,size=1,signed=F)
-    ret$x = matrix(x, ncol=nrow*ncol, byrow=T)
-    close(f)
-    ret
-  }
-  load_label_file <- function(filename) {
-    f = file(filename,'rb')
-    readBin(f,'integer',n=1,size=4,endian='big')
-    n = readBin(f,'integer',n=1,size=4,endian='big')
-    y = readBin(f,'integer',n=n,size=1,signed=F)
-    close(f)
-    y
-  }
+
   train <- load_image_file('mnist/train-images.idx3-ubyte')
   test <- load_image_file('mnist/t10k-images.idx3-ubyte')
   
   train$y <- load_label_file('mnist/train-labels.idx1-ubyte')
   test$y <- load_label_file('mnist/t10k-labels.idx1-ubyte')  
   mnist.train <<-data.frame(y=train$y, x=train$x)
-  mnist.test<<- data.frame(y=test$y, x=test$x)
+  mnist.test <<- data.frame(y=test$y, x=test$x)
 
 }
 
--- SL/hw5/preprocessor.R
+++ SL/hw5/preprocessor.R
@@ -6,7 +6,7 @@ library(raster)
 source("PCA/PCAfncs.R")
 source("mnist.r") # http://yann.lecun.com/exdb/mnist/
 load_mnist()
-train<- mnist.train[1:100, ]
+train<- mnist.train[1:10, ]
 mnist.train <- train
 test <- mnist.test[1:1000, ]
 mnist.test <- test
@@ -118,112 +118,21 @@ mnist.train$y <- factor(mnist.train$y)
 mnist.test <-as.data.frame(newtrain2)
 mnist.test$y <- factor(mnist.test$y)
 
+save_image_file('mnist/train-data.changed', mnist.train, nrow(mnist.train), as.integer(maxwidth), as.integer(maxheight))
+save_label_file('mnist/train-labels.changed', nrow(mnist.train), mnist.train[,1])
 
-deskew <- function(df, mincol=1, maxcol=ncol(df), threshold=1.10) {
-  for (i in mincol:maxcol) {
-    t <- log(1+df[i])[, 1]#min(df[i])
-    if (is.nan(skewness(t, na.rm=TRUE)))
-      next 
-    if (abs(skewness(df[,i], na.rm=TRUE)) > threshold * abs(skewness(t, na.rm=TRUE)))
-      df[i] <- t
-  }
-  df
-}
-pcalda <- function(...) pcawrap(lda, ...)
-predict.pcalda <- function(...) predict(...)$class
 show_digit <- function(arr784, col = gray(12:1/12), ...) {
-    image(matrix(arr784, nrow = maxheight)[, maxheight:1], col = col, ...)
+  image(matrix(arr784, nrow = maxheight)[, maxheight:1], col = col, ...)
 }
-
-#for (i in 1:nrow(mnist.train)) {
-#  r <- raster(matrix(t(mnist.train[i, -1]), nrow = 28))
-#  r3 <- focal(r, w=matrix(1/9,nrow=3,ncol=3))
-#  mm <- as.matrix(r3)
-#  mm[is.na(mm)]<-0
-#  mnist.train[i, -1] <- mm
-#}
-
-
-#for (i in 1:nrow(mnist.test)) {
-#  r <- raster(matrix(t(mnist.test[i, -1]), nrow = 28))
-#  r3 <- focal(r, w=matrix(1/9,nrow=3,ncol=3))
-#  mm <- as.matrix(r3)
-#  mm[is.na(mm)]<-0
-#  mnist.test[i, -1] <- as.matrix(r3)
-#}
-
 show_digit(as.matrix(mnist.train[1, -1]), main = mnist.train[1, 1])
 show_digit(as.matrix(mnist.train[2, -1]), main = mnist.train[2, 1])
-show_digit(as.matrix(mnist.train[11, -1]), main = mnist.train[11, 1])
+show_digit(as.matrix(mnist.train[3, -1]), main = mnist.train[3, 1])
 
-sds <- sapply(mnist.train, sd)
-zero.sd <- names(mnist.train)[sds < 2 ]
-mnist.train.nz <- mnist.train[, setdiff(names(mnist.train), zero.sd)]
+train <- load_image_file2('mnist/train-data.changed')
+train.y <- load_label_file('mnist/train-labels.changed')
 
-#sds2 <- sapply(mnist.train.nz, sd)
-#zero.sd2 <- names(mnist.train.nz)[sds2 < 2 ][-1]
-#mnist.train.nz2 <- mnist.train.nz[, setdiff(names(mnist.train.nz), zero.sd2)]
-#mnist.train.nz <- mnist.train.nz2 
-
-mnist.train.nz <- deskew(mnist.train.nz, mincol=2)
-
-#sds2 <- sapply(mnist.train.nz, sd)
-#zero.sd2 <- names(mnist.train.nz)[(sds2) <= 2]
-#mnist.train.nz <- mnist.train.nz [, setdiff(names(mnist.train.nz), zero.sd2)]
-
-mnist.train.nz.jittered <- mnist.train.nz
-mnist.train.nz.jittered[, -1] <- apply(mnist.train.nz[, -1], 2, jitter)
-
-tn.lda <- tune(lda, y ~ ., data = mnist.train.nz.jittered,
-               predict.func = predict.pcalda, tunecontrol = tune.control(cross = 2))
-summary(tn.lda)
-table(actual = mnist.train$y, predicted = predict(tn.lda$best.model, mnist.train)$class)
-
-tt <- table(actual = mnist.test$y, predicted = predict(tn.lda$best.model,
-                                                       mnist.test)$class)
-print(tt)
-tn.lda$best.model
-
-
-#zero.sd <- names(mnist.train)[sds < 2]
-#mnist.train.nz <- mnist.train[, setdiff(names(mnist.train), zero.sd)]
-#mnist.train.nz<- deskew(mnist.train.nz, mincol=2)
-pcaqda <- function(...) pcawrap(qda, ...)
-predict.pcaqda <- function(...) predict(...)$class
-
-
-
-tn.qda <- tune(qda, y ~ ., data = mnist.train.nz.jittered,
-               predict.func = predict.pcaqda,
-               tunecontrol = tune.control(cross = 2))
-
-summary(tn.qda)
-
-tn.pcaqda <- tune(pcaqda, y ~ ., data = mnist.train,
-                  scale = FALSE, center = TRUE,# ranges = list(ncomp = c(1, 10, 20, 40, 50)),
-                  ranges = list(ncomp = c(10, 30, 35, 40, 50, 70, 90)),
-                  predict.func = predict.pcaqda,
-                  tunecontrol = tune.control(cross = 2))
-summary(tn.pcaqda)
-
-plot(tn.pcaqda)
-
-table(actual = mnist.train$y,
-      predicted = predict(tn.pcaqda$best.model)$class)
-
-tt <- table(actual = mnist.test$y,
-            predicted = predict(tn.pcaqda$best.model, mnist.test)$class)
-print(tt)
-
-1 - sum(diag(tt)) / sum(tt)
-
-prs <- by(mnist.train, mnist.train$y, function(df) {
-  pr <- prcomp(~. - y, data = df, scale = FALSE,
-               center = TRUE, ncomp = 3)
-})
-show_digit(prs[["0"]]$rotation[, 1])
-show_digit(prs[["1"]]$rotation[, 1])
-show_digit(prs[["3"]]$rotation[, 1])
-show_digit(prs[["4"]]$rotation[, 1])
-show_digit(prs[["7"]]$rotation[, 1])
+show_digit(matrix(train$x[1,], nrow=train$nrow), main = train.y[1])
+show_digit(matrix(train$x[2,], nrow=train$nrow), main = train.y[2])
+show_digit(matrix(train$x[8,], nrow=train$nrow), main = train.y[8])
+  
 
