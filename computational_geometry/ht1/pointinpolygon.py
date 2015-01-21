4f30d9c134afe62ac3354e1f264afccb9e85d971
Tatiana Malygina <merlettaia@gmail.com>
2015-01-21 08:57:39 +0300

    dummy changes of old ht

--- computational_geometry/ht1/pointinpolygon.py
+++ computational_geometry/ht1/pointinpolygon.py
@@ -3,11 +3,52 @@ import sys
 import re
 
 class Polygon(object):
+  OFFSET = 1
   def __init__(self, points):
     self.points = points
-    print "in init"
+    self.prepare_ray_outer_point()
+    #print self.bounding_box
+  def crosses(self, ray1, ray2):
+    test_result = self.intersection_test(ray1[0], ray1[1], ray2[0], ray2[1])
+    if not test_result:
+      return [test_result, []]
+    return [True, []]
+    print "check1"
+  def prepare_ray_outer_point(self):
+    self.bounding_box = [[f(x[i] for x in self.points) for i in [0, 1]] for f in [min, max]]
+    self.ray_outer_point = [x - self.OFFSET for x in self.bounding_box[0]]
+    return self.ray_outer_point
+  #returns True if point is inside of given polynom
+  #returns False otherwise
   def hit_test(self, point):
+    if point[0] < self.bounding_box[0][0] or point[0] > self.bounding_box[1][0] or point[1] < self.bounding_box[0][1] or point[1] > self.bounding_box[1][1]:
+      return False
+    test_ray = [self.ray_outer_point, point]
+    odd_no_of_intersections = False
+    for i in range(len(self.points)):
+      line_segment = [self.points[i - 1], self.points[i]]
+      [cross_result, intersection] = self.crosses(test_ray, line_segment)
+      if cross_result:
+        odd_no_of_intersections = not odd_no_of_intersections
+    return odd_no_of_intersections
+  def dot_product(self, p1, p2, p3, p4):
+    return [(p2[i] - p1[i])*(p4[i] - p3[i]) for i in [0, 1]].sum
+  def intersection_test2(self, box1, box2):
+    for i in [0, 1]:
+      if box1[0][i] < box2[0][i]:
+        if box1[1][i] < box2[0][i]:
+          return False
+      else:
+        if box2[1][i] < box1[0][i]:
+          return False
     return True
+  #fast intersection test for two given pairs of points
+  def intersection_test(self, p1, p2, p3, p4):
+    boxes = [
+      [[f(x1[i], x2[i]) for i in [0, 1]] for f in [min, max]]
+      for x1, x2 in [[p1, p2], [p3, p4]]
+    ]
+    return self.intersection_test2(boxes[0], boxes[1])
 
 def load_point(str):
   r = re.compile('[\-0-9]+')
