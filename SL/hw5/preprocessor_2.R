aa0aa2058a51ed1adcf9a4ee69c1160a1348659b
Tatiana Malygina <merlettaia@gmail.com>
2015-01-23 09:51:15 +0300

    logic fix for processor2

--- SL/hw5/preprocessor_2.R
+++ SL/hw5/preprocessor_2.R
@@ -28,29 +28,33 @@ check.connected <- function(digit) {
   #following helper method simply fills connected area with 0 value, from point [r, c]
   newdigit <<- matrix(digit, nrow = maxheight)
   colorer <- function(r, c, lvl = 0) {
+    size<-0
     if (newdigit[r, c] == 0) {
         newdigit[r, c] <<- 1
+        size <- size + 1
         rp1 <- if (r == nrow(newdigit)) 1 else r + 1
         rm1 <- if (r == 1) nrow(newdigit) else r - 1
         cp1 <- if (c == ncol(newdigit)) 1 else c + 1
         cm1 <- if (c == 1) ncol(newdigit) else c - 1
-        colorer(rm1, c, lvl + 1)
-        colorer(rp1, c, lvl + 1)
-        colorer(r, cm1, lvl + 1)
-        colorer(r, cp1, lvl + 1)
-        colorer(rp1, cp1, lvl + 1)
-        colorer(rp1, cm1, lvl + 1)
-        colorer(rm1, cp1, lvl + 1)
-        colorer(rm1, cm1, lvl + 1)
+        size <- size + colorer(rm1, c, lvl + 1)
+        size <- size + colorer(rp1, c, lvl + 1)
+        size <- size + colorer(r, cm1, lvl + 1)
+        size <- size + colorer(r, cp1, lvl + 1)
+        size <- size + colorer(rp1, cp1, lvl + 1)
+        size <- size + colorer(rp1, cm1, lvl + 1)
+        size <- size + colorer(rm1, cp1, lvl + 1)
+        size <- size + colorer(rm1, cm1, lvl + 1)
     }
+    size
   }
 
   ncomp <- 0
   for (i in 1: nrow(newdigit)) {
     for (j in 1: ncol(newdigit)) {
       if (newdigit[i, j] == 0) {
-        ncomp <- ncomp + 1
-        colorer(i, j)
+        size <- colorer(i, j)
+        if (size >3)
+          ncomp <- ncomp + 1
       }
     }
   }
@@ -68,9 +72,9 @@ for (i in 1: nrow(mnist.test)) {
 }
 
 
-save_label_file('mnist/train-data.connected.comp', nrow(mnist.train), connected_comp.train)
+save_label_file('mnist/train-data.connected.comp.2', nrow(mnist.train), connected_comp.train)
 
-save_label_file('mnist/test-data.connected.comp', nrow(mnist.test), connected_comp.test)
+save_label_file('mnist/test-data.connected.comp.2', nrow(mnist.test), connected_comp.test)
 
 
 
