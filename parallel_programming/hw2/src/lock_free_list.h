830eee6499867b3568bccca4b3c7abcbb3786079
Tatiana Malygina <merlettaia@gmail.com>
2015-02-01 01:08:10 +0300

    fixed l1

--- parallel_programming/hw2/src/lock_free_list.h
+++ parallel_programming/hw2/src/lock_free_list.h
@@ -7,14 +7,13 @@ class LockFreeList: public SyncList<ElementType> {
   public:
     LockFreeList() {
         head = NULL;
-        tail = NULL;
     }
 
     void insert(ElementType& element, int key) {
-        std::cout << "insert called\n";
+        boost::mutex::scoped_lock lock(mutex);
+        //std::cout << "insert called\n";
         if (head == NULL) {
             head = std::make_shared<Node<ElementType> >(element, key);
-            tail = head;
             return;
         }
         if (head->key > key) {
@@ -22,10 +21,10 @@ class LockFreeList: public SyncList<ElementType> {
             return;
         }
         auto list_iterator = head;
-        for ( ; list_iterator != tail; list_iterator = list_iterator->next) {
-            if (list_iterator->next->key > key)
-                break;
+        for (; list_iterator != NULL; list_iterator = list_iterator->next) {
+            if (list_iterator->key >= key) break;
         }
+        if (list_iterator == NULL) return;
         if (list_iterator->key == key) {
             list_iterator->element = element;
             return;
@@ -33,34 +32,30 @@ class LockFreeList: public SyncList<ElementType> {
         list_iterator->next = std::make_shared<Node<ElementType> >(element, key, list_iterator->next);
     }
 
+
     void erase(int key) {
-        std::cout << "erase called\n";
-        if (head == NULL)
-            return;
+        boost::mutex::scoped_lock lock(mutex);
+        //std::cout << "erase called\n";
+        if (head == NULL) return;
+        if (head->key > key) return;
         if (head->key == key) {
-            if (head == tail) {
-                head = NULL;
-                tail = NULL;
-            } else {
-                head = head->next;
-            }
+            head = head->next;
         }
-        for (auto list_iterator = head; list_iterator != tail; list_iterator = list_iterator->next) {
+        for (auto list_iterator = head; list_iterator->next != NULL; list_iterator = list_iterator->next) {
             if (list_iterator->next->key == key) {
                 list_iterator->next = list_iterator->next->next;
             }
-            if (list_iterator->next->key > key)
-                break;
+            if (list_iterator->next->key > key) break;
         }
     }
 
     std::pair<bool, ElementType> find(int key) {
-        std::cout << "find called\n";
-        for (auto list_iterator = head; list_iterator != tail; list_iterator = list_iterator->next) {
-            if (list_iterator->key == key)
-                return std::make_pair(true, list_iterator->element);
-            if (list_iterator->key > key)
-                break;
+        boost::mutex::scoped_lock lock(mutex);
+        //std::cout << "find called\n";
+
+        for (auto list_iterator = head; list_iterator != NULL; list_iterator = list_iterator->next) {
+            if (list_iterator->key == key) return std::make_pair(true, list_iterator->element);
+            if (list_iterator->key > key) break;
         }
         return std::make_pair(false, ElementType());
     }
@@ -70,11 +65,11 @@ class LockFreeList: public SyncList<ElementType> {
     /*debug method*/
     void print(std::ostream& os) {
       os << "print list contents\n";
-      for (auto list_iterator = head; list_iterator!= tail; list_iterator = list_iterator->next) {
+      for (auto list_iterator = head; list_iterator!= NULL; list_iterator = list_iterator->next) {
         os << list_iterator->key << " ";
       }
       os <<"end of contents\n";
     }
   private:
-    std::shared_ptr<Node<ElementType > > head,  tail;
+    std::shared_ptr<Node<ElementType > > head;
 };
--- parallel_programming/hw2/src/main.cpp
+++ parallel_programming/hw2/src/main.cpp
@@ -6,17 +6,21 @@ namespace po = boost::program_options;
 #include <iostream>
 #include <iterator>
 #include <memory>
+#include <atomic>
 
-#include "synchronized_lists.h"
+#include "base.h"
+#include "lockable_list.h"
+#include "lock_free_list.h"
 
+std::atomic<long long> opcount;
 
-bool parse_cmd_options(int argc, char*argv[], int & readers_count, int & writers_count, int &operations_count, int & list_type) {
+bool parse_cmd_options(int argc, char*argv[], int & readers_count, int & writers_count, long long &operations_count, int & list_type) {
   po::options_description desc("Allowed options");
   desc.add_options()
   ("help", "produce help message")
   ("readers_count", po::value <int> (), "number of reader threads")
   ("writers_count", po::value <int>(), "number of writer threads")
-  ("operations_count", po::value <int>(), "number of operations")
+  ("operations_count", po::value <long long>(), "number of operations")
   ("list_type", po::value <int>(), "list type (0 -locking, otherwise - lock-free)")
   ;
   po::positional_options_description p;
@@ -39,9 +43,9 @@ bool parse_cmd_options(int argc, char*argv[], int & readers_count, int & writers
       vm.count("operations_count") &&
       vm.count("list_type")) {
         readers_count = vm["readers_count"].as<int>();
-    writers_count = vm["writers_count"].as<int>();
-    operations_count = vm["operations_count"].as<int>();
-    list_type = vm["list_type"].as<int>();
+        writers_count = vm["writers_count"].as<int>();
+        operations_count = vm["operations_count"].as<long long>();
+        list_type = vm["list_type"].as<int>();
   }
   else {
     std::cout  << "not all args were provided\n" << desc << "\n";
@@ -54,41 +58,70 @@ bool parse_cmd_options(int argc, char*argv[], int & readers_count, int & writers
 /* operation_type == 1 if writer, otherwise 0 (reader)
 */
 template <class ElementType>
-void do_smth(SyncList<ElementType>* list, int operation_type) {
+void do_smth(std::shared_ptr<SyncList<ElementType> > list, int operation_type, int i) {
   if (operation_type) {//write mode
+    while (opcount.load(std::memory_order_consume) > 0) {
+      opcount --;
+      int value = rand() % 1000;
+      bool optype = (rand() % 100) > 20;
+      std::cout << i <<  " ss2s\n";
 
+      // let's call add if given random number % 100 is greater than 20, else call erase
+      if (optype) {
+        list->insert(value, value);
+      }
+      else {
+        list->erase(value);
+      }
+      std::cout << i << " s3s\n";
+    }
   }
   else {//read mode
-
+    while (opcount.load(std::memory_order_consume) > 0) {
+      opcount --;
+      int key_to_find = rand() % 1000;
+      std::pair<bool, int> find_result = list->find(key_to_find);
+      std::cout << i << " \n";
+      //if (find_result->first) {
+      //  std::cout << "smth was found\n";
+      //}
+    }
   }
 }
 
 
 
 int main(int argc, char* argv[]) {
-  //try {
-  int rcount, wcount, opcount, list_type;
-  if (!parse_cmd_options(argc, argv, rcount, wcount, opcount, list_type)) return 1;
-  //std::cout << rcount <<" "<< wcount <<" "<< opcount << " "<< list_type;
-  boost::thread_group list_operators;
-  SyncList<int> * list;
-  if (list_type) {
-    list = new LockFreeList<int>();
-  } else {
-    list = new LockableList<int>();
-  }
-  for (int i = 0; i < rcount + wcount; i++) {
-    list_operators.add_thread(new boost::thread(do_smth<int>, list, i < wcount));
-  }
-  list_operators.join_all();
-  /*}
-  catch(std::exception& e) {
-  std::cerr << "error: " << e.what() << "\n";
-  return 1;
-}
-catch(...) {
-std::cerr << "Exception of unknown type!\n";
-}*/
+    srand(time(NULL));
 
-return 0;
+    //try {
+    int rcount, wcount, list_type;
+    long long operations_count;
+
+    if (!parse_cmd_options(argc, argv, rcount, wcount, operations_count, list_type)) return 1;
+    opcount = operations_count;
+    //std::cout << rcount <<" "<< wcount <<" "<< opcount << " "<< list_type;
+    boost::thread_group list_operators;
+    std::shared_ptr<SyncList<int> > list;
+    if (list_type) {
+        list = std::make_shared<LockFreeList<int> >();
+    } else {
+        list = std::make_shared<LockableList<int> >();
+    }
+    for (int i = 0; i < rcount + wcount; i++) {
+        list_operators.add_thread(new boost::thread(do_smth<int>, list, i < wcount, i));
+    }
+    list_operators.join_all();
+
+    //list->print(std::cout);
+    /*}
+    catch(std::exception& e) {
+    std::cerr << "error: " << e.what() << "\n";
+    return 1;
+    }
+    catch(...) {
+    std::cerr << "Exception of unknown type!\n";
+    }*/
+
+    return 0;
 }
