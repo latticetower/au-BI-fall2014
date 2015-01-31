01221145f268643ff3c038b85d3db4ad82a8b0bf
Tatiana Malygina <merlettaia@gmail.com>
2015-01-31 20:00:56 +0300

    wip

--- parallel_programming/hw2/src/main.cpp
+++ parallel_programming/hw2/src/main.cpp
@@ -51,14 +51,18 @@ bool parse_cmd_options(int argc, char*argv[], int & readers_count, int & writers
   return true;
 }
 
+/* operation_type == 1 if writer, otherwise 0 (reader)
+*/
 template <class ElementType>
-void write(SyncList<ElementType>* list) {
+void do_smth(SyncList<ElementType>* list, int operation_type) {
+  if (operation_type) {//write mode
+
+  }
+  else {//read mode
+
+  }
 }
 
-template <class ElementType>
-void read(SyncList<ElementType>* list) {
-
-}
 
 
 int main(int argc, char* argv[]) {
@@ -73,12 +77,8 @@ int main(int argc, char* argv[]) {
   } else {
     list = new LockableList<int>();
   }
-  for (int i = 0; i < rcount; i++) {
-    list_operators.add_thread(new boost::thread(read<int>, list));
-  }
-
-  for (int i = 0; i < wcount; i++) {
-    list_operators.add_thread(new boost::thread(write<int>, list));
+  for (int i = 0; i < rcount + wcount; i++) {
+    list_operators.add_thread(new boost::thread(do_smth<int>, list, i < wcount));
   }
   list_operators.join_all();
   /*}
--- parallel_programming/hw2/src/synchronized_lists.h
+++ parallel_programming/hw2/src/synchronized_lists.h
@@ -1,15 +1,27 @@
 #pragma once
+template<class ElementType>
+struct Node {
+  Node(ElementType& element_, int key_, Node<ElementType> * next_ = NULL)
+          : element(element_), key(key_), next(next_) {
+  }
+
+  ElementType element;
+  int key;
+  Node<ElementType>* next;
+
+};
 
 template<class ElementType>
 class SyncList{
 public:
-  virtual void insert() {
+  //method returns actual key
+  virtual void insert(ElementType & element, int key) {
 
   }
   virtual void erase() {
 
   }
-  virtual void find() {
+  std::pair<bool, ElementType&> find() {
 
   }
   virtual ~SyncList() {}
@@ -19,39 +31,130 @@ template<class ElementType>
 class LockableList: public SyncList<ElementType>{
   public:
     LockableList() {
-
+        head = NULL;
+        tail = NULL;
     }
-    void insert() {
-
+    void insert(ElementType& element, int key) {
+        if (head == NULL) {
+          head = new Node<ElementType>(element, key);
+          tail = head;
+          return;
+        }
+        if (head->key > key) {
+          head = new Node<ElementType>(element, key, head);
+          return;
+        }
+        auto list_iterator = head;
+        for (; list_iterator != tail; list_iterator = list_iterator->next) {
+          if (list_iterator->next->key > key)
+            break;
+        }
+        if (list_iterator->key == key) {
+          list_iterator->element = element;
+          return;
+        }
+        list_iterator->next = new Node<ElementType>(element, key, list_iterator->next);
     }
-    void erase() {
-
+    void erase(int key) {
+      if (head->key == key) {
+        auto temp = head;
+        if (head == tail) {
+          head = NULL;
+          tail = NULL;
+        } else {
+          head = head->next;
+        }
+        delete temp;
+      }
+      for (auto list_iterator = head; list_iterator != tail; list_iterator = list_iterator->next) {
+        if (list_iterator->next->key == key) {
+          auto temp = list_iterator->next;
+          list_iterator->next = temp->next;
+          delete temp;
+        }
+        if (list_iterator->next->key > key)
+          break;
+      }
     }
-    void find() {
-
+    std::pair<bool, ElementType&> find(int key) {
+      for (auto list_iterator = head; list_iterator != tail; list_iterator = list_iterator->next) {
+        if (list_iterator->key == key)
+          return std::make_pair(true, list_iterator->element);
+        if (list_iterator->key > key)
+          break;
+      }
+      return std::make_pair(false, ElementType());
     }
     ~LockableList() {
 
     }
+  private:
+    Node<ElementType> * head,
+                      * tail;
 };
 
 template<class ElementType>
 class LockFreeList: public SyncList<ElementType> {
   public:
     LockFreeList() {
-
+      head = NULL;
+      tail = NULL;
     }
-    void insert() {
-
-    }
-    void erase() {
-
-    }
-    void find() {
-
+    void insert(ElementType& element, int key) {
+      if (head == NULL) {
+        head = new Node<ElementType>(element, key);
+        tail = head;
+        return;
+      }
+      if (head->key > key) {
+        head = new Node<ElementType>(element, key, head);
+        return;
+      }
+      auto list_iterator = head;
+      for ( ; list_iterator != tail; list_iterator = list_iterator->next) {
+        if (list_iterator->next->key > key)
+          break;
+        }
+        if (list_iterator->key == key) {
+          list_iterator->element = element;
+          return;
+        }
+        list_iterator->next = new Node<ElementType>(element, key, list_iterator->next);
+      }
+      void erase(int key) {
+        if (head->key == key) {
+          auto temp = head;
+          if (head == tail) {
+            head = NULL;
+            tail = NULL;
+          } else {
+            head = head->next;
+          }
+          delete temp;
+        }
+        for (auto list_iterator = head; list_iterator != tail; list_iterator = list_iterator->next) {
+          if (list_iterator->next->key == key) {
+            auto temp = list_iterator->next;
+            list_iterator->next = temp->next;
+            delete temp;
+          }
+          if (list_iterator->next->key > key)
+            break;
+          }
+        }
+    std::pair<bool, ElementType&> find(int key) {
+        for (auto list_iterator = head; list_iterator != tail; list_iterator = list_iterator->next) {
+          if (list_iterator->key == key)
+            return std::make_pair(true, list_iterator->element);
+          if (list_iterator->key > key)
+            break;
+        }
+        return std::make_pair(false, ElementType());
     }
 
     ~LockFreeList() {
 
     }
+  private:
+    Node<ElementType > * head, * tail;
 };
