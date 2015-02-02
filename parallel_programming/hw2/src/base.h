d88a3f45c0d51e9a29635221e819b51035b1df15
Tatiana Malygina <merlettaia@gmail.com>
2015-02-02 22:06:32 +0300

    updated base.h

--- parallel_programming/hw2/src/base.h
+++ parallel_programming/hw2/src/base.h
@@ -23,30 +23,32 @@ template<class ElementType>
 struct MarkedAtomicNode {
   MarkedAtomicNode(ElementType element_, int key_,
           std::shared_ptr<MarkedAtomicNode<ElementType> > next_ = NULL,
-          bool marked_ = false) : element(element_), key(key_), marked(marked_), next(next_) {
-  }
-  MarkedAtomicNode() {
-    element = ElementType();
-    key = 0;
-    marked = false;
-    next = NULL;
-  }
-  std::shared_ptr<MarkedAtomicNode<ElementType> > get_next_ptr() {
-      std::shared_ptr<MarkedAtomicNode<ElementType> > next_node = next.load();
-      return next;
+          bool marked_ = false)
+              : data(Node<std::pair<ElementType, bool> >(std::make_pair(element_, marked_), key_, next_)) {
   }
 
-  ElementType element;
-  int key;
-  bool marked;
-  boost::atomic<std::shared_ptr<MarkedAtomicNode<ElementType> > > next;
+  MarkedAtomicNode()
+      : data(Node<std::pair<ElementType, bool> >(std::make_pair(ElementType(), false,), 0, NULL)) {
+  }
+
+  boost::atomic<Node<std::pair<ElementType, bool> > data;
+
+  ElementType element() {
+      return data.load().element.first;
+  }
+
+  std::shared_ptr<MarkedAtomicNode<ElementType> > next() {
+      return data.load().next;
+  }
+  bool key() {
+    return data.load().key;
+  }
+
+  bool marked() {
+    return data.load().element.second;
+  }
+
 };
-
-/* method returns object ptr for given atomic*/
-template<class ElementType>
-std::shared_ptr<MarkedAtomicNode<ElementType> > get_ptr(boost::atomic<std::shared_ptr<MarkedAtomicNode<ElementType> > >& atomic_node) {
-  return atomic_node.load();
-}
 
 /* method returns copy of newly created object ptr with modified next and marked fields*/
 template<class ElementType>
@@ -58,11 +60,6 @@ std::shared_ptr<MarkedAtomicNode<ElementType> > construct_new_from_atomic(
     return std::make_shared<MarkedAtomicNode<ElementType> >(node->element, node->key, new_mark, new_ptr.load());
 }
 
-/* method returns key for given atomic*/
-template<class ElementType>
-int get_key(boost::atomic<std::shared_ptr<MarkedAtomicNode<ElementType> > >& atomic_node) {
-  return atomic_node.load()->key;
-}
 
 /* method loads next and marked value to corresponding variables from given atomic obj ptr*/
 template<class ElementType>
@@ -76,9 +73,9 @@ void load_value(boost::atomic<std::shared_ptr<MarkedAtomicNode<ElementType> > >&
 
 template<class ElementType>
 bool compareAndSet(
-        boost::atomic<std::shared_ptr<MarkedAtomicNode<ElementType> > >& atomic_node,
-        boost::atomic<std::shared_ptr<MarkedAtomicNode<ElementType> > >& next_ptr,
-        boost::atomic<std::shared_ptr<MarkedAtomicNode<ElementType> > >& new_next_ptr,
+        std::shared_ptr<MarkedAtomicNode<ElementType> >& atomic_node,
+        std::shared_ptr<MarkedAtomicNode<ElementType> >& next_ptr,
+        std::shared_ptr<MarkedAtomicNode<ElementType> >& new_next_ptr,
         bool next_mark = false,
         bool new_next_mark = false
         ) {
