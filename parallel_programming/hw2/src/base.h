e60f4a2ca99718af3b88d26b2b5ef2c299e381d1
Tatiana Malygina <merlettaia@gmail.com>
2015-02-03 16:41:56 +0300

    fixed base classes

--- parallel_programming/hw2/src/base.h
+++ parallel_programming/hw2/src/base.h
@@ -2,39 +2,58 @@
 
 #include<boost/atomic.hpp>
 
-
 template<class ElementType>
 struct Node {
-    Node(ElementType element_, int key_, std::shared_ptr<Node<ElementType> > next_ = NULL)
-        : element(element_), key(key_), next(next_) {
-    }
-    Node() {
-        element = ElementType();
-        key = 0;
-        next = NULL;
-    }
+  Node(ElementType element_, int key_, std::shared_ptr<Node<ElementType> > next_ = NULL)
+  : element(element_), key(key_), next(next_) {
+  }
+  Node() {
+    element = ElementType();
+    key = 0;
+    next = NULL;
+  }
 
-    ElementType element;
-    int key;
-    std::shared_ptr<Node<ElementType> > next;
+  ElementType element;
+  int key;
+  std::shared_ptr<Node<ElementType> > next;
 };
+
+template<class ElementType, class NextElementType >
+struct MarkedNode {
+  MarkedNode(ElementType element_, int key_, std::shared_ptr<NextElementType > next_ = NULL, bool marked_ = false)
+  : element(element_), key(key_), marked(marked_), next(next_) {
+  }
+  MarkedNode() {
+    element = ElementType();
+    key = 0;
+    marked = false;
+    next = NULL;
+  }
+
+  ElementType element;
+  int key;
+  bool marked;
+  std::shared_ptr<NextElementType > next;
+};
+
 
 template<class ElementType>
 struct MarkedAtomicNode {
+  typedef MarkedNode<ElementType, MarkedAtomicNode<ElementType> > InnerNode;
   MarkedAtomicNode(ElementType element_, int key_,
           std::shared_ptr<MarkedAtomicNode<ElementType> > next_ = NULL,
           bool marked_ = false)
-              : data(Node<std::pair<ElementType, bool> >(std::make_pair(element_, marked_), key_, next_)) {
+              : data(InnerNode(element_, key_, next_, marked_)) {
   }
 
   MarkedAtomicNode()
-      : data(Node<std::pair<ElementType, bool> >(std::make_pair(ElementType(), false,), 0, NULL)) {
+      : data(InnerNode(ElementType(), 0, NULL, false)) {
   }
 
-  boost::atomic<Node<std::pair<ElementType, bool> > data;
+  boost::atomic<InnerNode> data;
 
   ElementType element() {
-      return data.load().element.first;
+      return data.load().element;
   }
 
   std::shared_ptr<MarkedAtomicNode<ElementType> > next() {
@@ -45,7 +64,7 @@ struct MarkedAtomicNode {
   }
 
   bool marked() {
-    return data.load().element.second;
+    return data.load().marked;
   }
 
 };
